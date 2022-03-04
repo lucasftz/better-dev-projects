92c48e9e4c7470c5524897501ce13735dc4a50af
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-04 18:01:50 -0500

    Create markdown state
    Change the state according to the text in the textarea.

--- markdown-editor/src/App.js
+++ markdown-editor/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [markdown, setMarkdown] = useState('');
+
   return (
-    <div className="App">
-      dummy text
+    <div className="app">
+      <textarea
+        value={markdown}
+        onChange={e => setMarkdown(e.target.value)}
+      />
+
+      <div className="preview">Render markdown here</div>
     </div>
   );
 }
