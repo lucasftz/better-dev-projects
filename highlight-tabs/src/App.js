a6d9a9e41fa422450eb680cda34124e2a24cd30a
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-05 18:10:59 -0500

    Separate each tab div into its own component

--- highlight-tabs/src/App.js
+++ highlight-tabs/src/App.js
@@ -1,41 +1,15 @@
 import React, { useState } from 'react';
+import Tab from './Tab'
 import './App.css';
 
 function App() {
-  const [highlightStyle, setHighlightStyle] = useState({left: 0, opacity: 0});
-
-  const moveHighlight = (e) => {
-    // update highlightStyle to move the highlight
-    setHighlightStyle({
-      // e.nativeEvent grabs the actual event instead of a synthetic event
-      // (which is what React wraps over the actual event)
-      // layerX is the pos of the mouse relative to the closest element, which is the tab div
-      // -150 because that's the width of of the highlight
-      left: e.nativeEvent.layerX - 150
-    })
-  }
-
-  const hideHighlight = (e) => {
-    setHighlightStyle({
-      left: e.nativeEvent.layerX - 150,
-      opacity: 0
-    })
-  }
-
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab" onMouseMove={moveHighlight} onMouseOut={hideHighlight}>
-            <div className="highlight" style={highlightStyle} />
-            <a>Home</a>
-          </div>
-          <div className="tab">
-            <a>About</a>
-          </div>
-          <div className="tab">
-            <a>Features</a>
-          </div>
+          <Tab props={{title: "Home"}} />
+          <Tab props={{title: "About"}} />
+          <Tab props={{title: "Features"}} />
         </div>
 
         <div className="viewport">Pages Go Here</div>
