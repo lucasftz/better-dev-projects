02346b79b1ecba3df41558630f50d7b3ac17816a
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-05 18:31:42 -0500

    Create simple route and pages components

--- highlight-tabs/src/App.js
+++ highlight-tabs/src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Tab from './components/Tab'
 import './App.css';
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Features from './pages/Features';
 
 function App() {
   return (
-    <div className="app">
-      <div className="browser">
-        <div className="tabs">
-          <Tab props={{title: "Home"}} />
-          <Tab props={{title: "About"}} />
-          <Tab props={{title: "Features"}} />
-        </div>
+    <Router>
+      <div className="app">
+        <div className="browser">
+          <div className="tabs">
+            <Tab props={{title: "Home"}} />
+            <Tab props={{title: "About"}} />
+            <Tab props={{title: "Features"}} />
+          </div>
 
-        <div className="viewport">Pages Go Here</div>
+          <div className="viewport">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/features" element={<Features />} />
+            </Routes>
+          </div>
+        </div>
       </div>
-    </div>
+    </Router>
+    
   );
 }
 
---
+++ highlight-tabs/src/pages/Features.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Features() {
+  return (
+    <div>
+      This is the features page
+    </div>
+  )
+}
+
+export default Features
