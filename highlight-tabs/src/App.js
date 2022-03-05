ef99dbcdb58faccba02083cf8d851a5430813fc9
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-05 17:26:39 -0500

    Add highlight effect to tabs

--- highlight-tabs/src/App.js
+++ highlight-tabs/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [highlightStyle, setHighlightStyle] = useState({left: 0});
+
+  const moveHighlight = (e) => {
+    // update highlightStyle to move the highlight
+    setHighlightStyle({
+      // e.nativeEvent grabs the actual event instead of a synthetic event
+      // (which is what React wraps over the actual event)
+      // layerX is the pos of the mouse relative to the closest element, which is the tab div
+      // -150 because that's the width of of the highlight
+      left: e.nativeEvent.layerX - 150
+    })
+  }
+
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
+          <div className="tab" onMouseMove={moveHighlight}>
+            <div className="highlight" style={highlightStyle} />
             <a>Home</a>
           </div>
           <div className="tab">
