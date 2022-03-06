fb56687d073df1be3a9a375bc475d070954c24d2
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-05 20:52:50 -0500

    Display wins/losses based on state

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -13,6 +13,8 @@ const choices = [
 
 
 function App() {
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
   const [userChoice, setUserChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
 
@@ -35,13 +37,13 @@ function App() {
         {/* wins vs losses stats */}
         <div className="wins-losses">
           <div className="wins">
-            <span className="number">0</span>
-            <span className="text">Wins</span>
+            <span className="number">{wins}</span>
+            <span className="text">{wins===1 ? "Win" : "Wins"}</span>
           </div>
 
           <div className="losses">
-            <span className="number">0</span>
-            <span className="text">Losses</span>
+            <span className="number">{losses}</span>
+            <span className="text">{losses===1 ? "Loss" : "Losses"}</span>
           </div>
         </div>
       </div>
