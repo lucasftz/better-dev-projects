decc6ec914fce5f9434260028f820d2a6d79bc7c
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-05 22:34:36 -0500

    Paste initial files

---
+++ image-gallery/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Unsplash Image Gallery!</h1>
+
+      <form>
+        <input type="text" placeholder="Search Unsplash..." />
+        <button>Search</button>
+      </form>
+
+      <div className="image-grid">
+        {[...Array(100)].map((_, index) => (
+          <div className="image" key={index}>
+            <img src="https://placekitten.com/g/1920/1080" alt="Sample" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
