d243faa6d1f5dfc6b63db3e4cec6e6b699692cee
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-08 18:58:53 -0500

    Convert tags into styled components

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 import './App.css';
 
 const calendarDates = Array(31)
@@ -34,37 +35,86 @@ function App() {
   };
 
   return <>
-    <div className="date-chooser">
-      <button
-        className="date-chooser-button"
+    <StyledDateChooser>
+      <StyledDateChooserBtn
         onClick={() => setChoosingType('start')}
       >
         Start Date <span>{startDate}</span>
-      </button>
-      <button
-        className="date-chooser-button"
+      </StyledDateChooserBtn>
+
+      <StyledDateChooserBtn
         onClick={() => setChoosingType('end')}
       >
         End Date <span>{endDate}</span>
-      </button>
-    </div>
+      </StyledDateChooserBtn>
+    </StyledDateChooser>
 
-    <div className="calendar">
+    <StyledCalendar>
       {calendarDates.map((day, index) => {
         const dayNumber = day + 1;
 
+        let isSelected = dayNumber === startDate || dayNumber === endDate;
+
         return (
-          <button
+          <StyledCalendarDay
             key={index}
-            className="calendar-day"
+            isSelected={isSelected}
             onClick={() => updateDate(dayNumber)}
           >
             {dayNumber + 1}
-          </button>
+          </StyledCalendarDay>
         )
       })}
-    </div>
+    </StyledCalendar>
   </>
 }
 
 export default App;
+
+const StyledDateChooser = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const StyledDateChooserBtn = styled.button`
+  color: #0b204c;
+  text-transform: uppercase;
+  flex: 1;
+  padding: 15px;
+  background: none;
+  cursor: pointer;
+  border: none;
+  border-bottom: 2px solid rgba(11, 32, 76, 0.2);
+  outline: none;
+
+  span {
+    display: block;
+    min-height: 60px;
+    font-size: 50px;
+  }
+`;
+
+const StyledCalendar = styled.div`
+  max-width: 400px;
+  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
+    0 10px 10px -5px rgba(0, 0, 0, 0.04);
+  background: #0b204c;
+  color: #fff;
+  padding: 20px;
+`;
+
+const StyledCalendarDay = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  transition: 0.3s ease background;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  color: #8096c1;
+  background: none;
+`;
