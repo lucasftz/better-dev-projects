9ed58f44a4514151810a8959c7180cbdb6d9c90f
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-08 18:35:04 -0500

    Allow user to choose start and end dates

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const calendarDates = Array(31)
@@ -6,19 +6,42 @@ const calendarDates = Array(31)
   .map((e, i) => i);
 
 function App() {
+  const [choosingType, setChoosingType] = useState('start'); // start or end
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const updateDate = (selectedDay) => {
+    if (choosingType === 'start') {
+      setStartDate(selectedDay);
+      setChoosingType('end');
+    } else {
+      setEndDate(selectedDay);
+    };
+  };
+
   return <>
     <div className="date-chooser">
       <button className="date-chooser-button">
-        Start Date <span>0</span>
+        Start Date <span>{startDate}</span>
       </button>
       <button className="date-chooser-button">
-        End Date <span>0</span>
+        End Date <span>{endDate}</span>
       </button>
     </div>
 
     <div className="calendar">
       {calendarDates.map((day, index) => {
-        return <div className="calendar-day">{day + 1}</div>;
+        const dayNumber = day + 1;
+
+        return (
+          <button
+            key={index}
+            className="calendar-day"
+            onClick={() => updateDate(dayNumber)}
+          >
+            {dayNumber + 1}
+          </button>
+        )
       })}
     </div>
   </>
