e21b37751e5cfe7a010dee26cf6f17fa7dfffae6
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-08 12:03:22 -0500

    Grab user info and check if authenticated on initial render

--- authentication/src/App.js
+++ authentication/src/App.js
@@ -7,23 +7,9 @@ import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 // context
 import { Auth0Context } from './contexts/auth0-context';
-// SPA client
-import createAuth0Client from '@auth0/auth0-spa-js';
 
 export default function App() {
   const auth0 = useContext(Auth0Context);
-
-  // initialize auth0 on initial render
-  useEffect(() => {
-    initAuth0();
-
-    async function initAuth0() {
-      const auth0 = await createAuth0Client({
-        domain: 'dev-0kausynv.us.auth0.com',
-        client_id: 'WfcCidddR9i47wvtME4tOcqzQON7meQ2'
-      });
-    };
-  }, []);
 
   return (
     <Router>
--- authentication/src/contexts/auth0-context.js
+++ authentication/src/contexts/auth0-context.js
@@ -1,10 +1,46 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import createAuth0Client from '@auth0/auth0-spa-js';
 
 const Auth0Context = createContext();
 
 function Auth0Provider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+  const [auth0Client, setAuth0Client] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    initAuth0();
+
+    async function initAuth0() {
+      const auth0 = await createAuth0Client({
+        domain: 'dev-0kausynv.us.auth0.com',
+        client_id: 'WfcCidddR9i47wvtME4tOcqzQON7meQ2'
+      });
+
+      setAuth0Client(auth0);
+
+      // check if user is authenticated
+      const isAuthenticated = await auth0.isAuthenticated();
+      setIsAuthenticated(isAuthenticated);
+
+      // grab the user
+      if (isAuthenticated) {
+        const user = await auth0.getUser();
+        setUser(user);
+      }
+
+      // stop loading
+      setIsLoading(false);
+    };
+  }, []);
+
   return (
-    <Auth0Context.Provider>
+    <Auth0Context.Provider value={{
+        isAuthenticated: isAuthenticated,
+        user: user,
+        isLoading: isLoading
+      }}>
       {children}
     </Auth0Context.Provider>
   );
