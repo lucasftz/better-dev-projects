df78b600b2658f5aa5576ebc2e98e08bae11ba07
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-08 11:34:07 -0500

    Initialize auth0 on initial render

--- authentication/src/App.js
+++ authentication/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SiteHeader from './components/SiteHeader';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import './App.css';
+// SPA client
+import createAuth0Client from '@auth0/auth0-spa-js';
 
 export default function App() {
+
+  // initialize auth0 on initial render
+  useEffect(() => {
+    initAuth0();
+
+    async function initAuth0() {
+      const auth0 = await createAuth0Client({
+        domain: 'dev-0kausynv.us.auth0.com',
+        client_id: 'WfcCidddR9i47wvtME4tOcqzQON7meQ2'
+      });
+    };
+  }, []);
+
   return (
     <Router>
       <div className="app">
