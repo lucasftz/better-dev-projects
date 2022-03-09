f3304a7b0e2e9ec2b4497103365ce2b9b37bbb78
Lucas Fietkiewicz <lucas.fietkiewicz>
2022-03-09 18:42:10 -0500

    More styling

--- src/home-page/HomePage.js
+++ src/home-page/HomePage.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './HomePage.css'
 
 const HomePage = () => {
-  return <div className="home-page">
-    <h1>Hello world!</h1>
-  </div>
+  return (
+    <div className="home-page">
+      <h1>Hey, I'm Lucas!</h1>
+      <h2>Want to see my work?</h2>
+    </div>
+  );
 };
 
 export default HomePage
